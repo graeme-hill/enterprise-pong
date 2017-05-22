d63f20a0f4f722e4204ca2df16e47ec264be543e
Graeme Hill <graemekh@gmail.com>
2017-05-21 21:36:20 -0700

    Improve acpa.sh

--- acpa.sh
+++ acpa.sh
@@ -1,13 +1,16 @@
 function addCommitPush {
-   pushd $1
+   pushd `git rev-parse --show-toplevel`/$1
    git add .
    git commit
    git push origin HEAD
    popd
 }
 
-addCommitPush `git rev-parse --show-toplevel`
-
 #git config --file .gitmodules --get-regexp path | awk '{ print $2 }' \
 #    | while read in; do addCommitPush $in; done
+
+addCommitPush modules/cg
+addCommitPush modules/cr
+addCommitPush .
+
 
