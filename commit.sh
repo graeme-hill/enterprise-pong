f4c8e830339acee371b7c321cb3b54fa689d6a54
Graeme Hill <graemekh@gmail.com>
2017-05-23 10:31:59 -0700

    Fix bug in commit.sh

--- commit.sh
+++ commit.sh
@@ -2,13 +2,6 @@ rm -rf .backup
 mkdir -p .backup
 cp -R * .backup
 
-function addCommitPush {
-   git add . && \
-   git commit && \
-   git push origin HEAD
-}
-
-git submodule foreach addCommitPush
-addCommitPush
+git submodule foreach "git add .; git commit; git push origin HEAD; return 0"
+git add .; git commit; git push origin HEAD
 git submodule foreach git branch
-
