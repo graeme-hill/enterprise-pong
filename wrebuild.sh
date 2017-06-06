76d04a60f12fbdd6a675dd43654c1cedcdea24be
Graeme Hill <graemekh@gmail.com>
2017-06-06 15:38:51 -0700

    Clean up build process`

--- wrebuild.sh
+++ wrebuild.sh
@@ -1,6 +1,6 @@
-rm -rf web-build
-mkdir web-build
-pushd web-build
+rm -rf wbuild
+mkdir wbuild
+pushd wbuild
 cmake -DJS=On -DCMAKE_C_COMPILER=`which emcc` \
 	-DCMAKE_CXX_COMPILER=`which em++` ..
 make -j9
