12d919e2db9cf66cd4ae980844d5361e144b9bea
Graeme Hill <graemekh@gmail.com>
2017-06-12 22:06:10 -0700

    Flatbuffer messages with schema

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,54 @@
 #include "Pong.hpp"
 #include "xs/Server.hpp"
+#include "xe/Backplane.hpp"
 
-#include "generated/GameMessage_generated.h"
+#include "generated/GameMessage.hpp"
+
+class Handler
+{
+public:
+	template<typename T>
+	void receive(T thing);
+
+	void receiveBuffer(flatbuffers::DetachedBuffer const &buffer);
+};
+
+void Handler::receiveBuffer(flatbuffers::DetachedBuffer const &buffer)
+{
+	auto p = msg::GetPayload(buffer.data());
+	p->message_receive(*this);
+}
+
+template <>
+inline void Handler::receive(msg::Goal const *g)
+{
+	std::cout << "goal\n";
+}
+
+template <typename T>
+inline void Handler::receive(T thing)
+{
+	std::cout << "unknown\n";
+}
 
 int main()
 {
-	flatbuffers::FlatBufferBuilder builder(1024);
+	xe::Backplane<msg::Payload, Handler> backplane;
+	flatbuffers::FlatBufferBuilder fbb(1024);
 
-	// capnp::MallocMessageBuilder message;
-	// GameMessage::Builder msg = message.initRoot<GameMessage>();
+	auto p = msg::Vec3(0, 0, 0);
+	auto b = msg::CreateGoal(fbb, 0, 0, 0, &p);
+	auto po = msg::CreatePayload(fbb, msg::Message_Goal, b.Union());
+	fbb.Finish(po);
+	auto bp = fbb.ReleaseBufferPointer();
+
+	backplane.post(std::move(bp));
+	Handler handler;
+
+	std::cout << "1\n";
+	backplane.receive(handler);
+	std::cout << "2\n";
+	backplane.receive(handler);
 
 	//xs::run();
 	xe::play<Pong>();
--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,10 @@
 rm -rf build
 mkdir build
 pushd build
-cmake -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ ..
+cmake \
+	-DCMAKE_C_COMPILER=/usr/bin/clang \
+	-DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+	-DCMAKE_BUILD_TYPE=Debug \
+	..
 make -j9
 popd
